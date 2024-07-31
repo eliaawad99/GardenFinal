670255325ecf24ea14356e00e90452b8e0274f55
Elia Awad <elia.t.awad@gmail.com>
2024-07-31 10:34:51 +0300

    web modification

--- routes/web.php
+++ routes/web.php
@@ -20,9 +20,7 @@ Route::middleware('auth')->group(function () {
     Route::get('/plants', [PlantController::class, 'index'])->name('plants.index');
     Route::post('/plants', [PlantController::class, 'store'])->name('plants.store');
     Route::post('/plants/{plantId}', [PlantController::class, 'update'])->name('plants.update');
-});
 
-Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::post('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
